606f565d1cecf644cb28411b251821ca9f117bcd
Truong Son <sonpaylak2006@gmail.com>
2026-09-15 15:56:44 +0700

    lab1

---
+++ lab1.js
@@ -0,0 +1,9 @@
+let name = "Nguyễn Văn An";
+let age = 20;
+let address = "Hà Nội";
+let isStudent = true;
+
+console.log("Họ tên:", name);
+console.log("Tuổi:", age);
+console.log("Địa chỉ:", address);
+console.log("Sinh viên:", isStudent);
