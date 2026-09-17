42bd93adf6cea8194eebebf205e5eab71b0f1a60
Truong Son <sonpaylak2006@gmail.com>
2026-09-17 15:24:16 +0700

    lab1

--- lab1.js
+++ lab1.js
@@ -1,3 +1,5 @@
+// Bài 1
+
 let name = "Nguyễn Văn An";
 let age = 20;
 let address = "Hà Nội";
@@ -7,3 +9,31 @@ console.log("Họ tên:", name);
 console.log("Tuổi:", age);
 console.log("Địa chỉ:", address);
 console.log("Sinh viên:", isStudent);
+
+// Bài 2
+
+console.log(age);
+
+age = 21;
+
+console.log(age);
+
+// Bài 3
+
+const a = 10;
+const b = 5;
+
+console.log("Tổng:", a + b);
+console.log("Hiệu:", a - b);
+console.log("Tích:", a * b);
+console.log("Thương:", a / b);
+
+// Bài 4
+
+function sayHello(name) {
+  console.log("Xin chào " + name);
+}
+
+sayHello("An");
+sayHello("Bình");
+sayHello("Cường");
