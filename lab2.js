7b83e076502f59cdbd3d57fb5923a83557d74e03
Truong Son <sonpaylak2006@gmail.com>
2026-09-19 15:46:06 +0700

    lab2

--- lab2.js
+++ lab2.js
@@ -18,3 +18,24 @@ console.log(names);
 for (let i = 0; i < names.length; i++) {
   console.log(names[i]);
 }
+
+// Bài 3: Tạo Object
+const student = {
+  id: 1,
+  name: "Nguyễn Văn An",
+  age: 20,
+  email: "an@gmail.com",
+  major: "CNTT",
+};
+
+// Bài 4: Array Object
+
+console.log(student);
+console.log(student.name);
+console.log(student.age);
+console.log(student.email);
+
+student.age = 21;
+student.phone = "0901234567";
+
+console.log(student);
