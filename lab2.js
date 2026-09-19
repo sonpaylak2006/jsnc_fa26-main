df144d97ef025cbc6f7b157f32485823957f341b
Truong Son <sonpaylak2006@gmail.com>
2026-09-19 15:15:45 +0700

    lab2

---
+++ lab2.js
@@ -0,0 +1,20 @@
+// Bài 1: Array cơ bản Tạo Array
+
+const names = ["An", "Bình", "Nam", "Hòa", "Lan"];
+
+console.log(names);
+console.log(names[0]);
+console.log(names[names.length - 1]);
+console.log(names.length);
+
+names.push("Minh");
+console.log(names);
+
+names.pop();
+console.log(names);
+
+// Bài 2: Duyệt Array
+
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
